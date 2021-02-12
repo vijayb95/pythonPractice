408d1ffc0930e886d0c365322260eb125e40cc17
Vijay <jayb2495@gmail.com>
2021-02-13 01:15:00 +0530

    end of day

--- 10 Amazing python projects/1_thesauras/thesauras.py
+++ 10 Amazing python projects/1_thesauras/thesauras.py
@@ -1,25 +1,53 @@
 import json
+from difflib import get_close_matches
 
-data = json.load(open('data.json'))
+def main():
+    data = json.load(open('data.json'))
 
-def keyCall():
-    return input('Enter the value you want to search in our dictionary: ')
+    def keyCall():
+        return input('Enter the key you want to search in our dictionary: ')
 
-searchKey = keyCall()
+    searchKey = keyCall()
 
-search = True
+    search = True
 
-def synonym(key):
-    key = key.lower()
-    translation = data[key]
-    if translation != '':
-        print(translation)
-    else: print('The Key doesn\'t exist in the dictionary, Please check your input.. \\\n')
+    def synonym(key):
+        translation = ''
+        try:
+            translation = data[key]
+        except:
+            newKeyFunc(key)
+            return
+        if translation != '':
+            printTranslation(translation)
+        else: 
+            return newKeyFunc(key)
+            
 
-while search:
-    if searchKey != '':
-        synonym(searchKey)
-        search = False
-    else: 
-        print('\nPlease enter a Value!\n')
-        searchKey = keyCall()
+    def newKeyFunc(key):
+        tempList = []
+        tempList = get_close_matches(key, data.keys(), cutoff=0.8)
+        if tempList == []:
+            print('The key doesn\'t exist in the dictionary, Please check your input.. \n')
+            return
+        newKey = tempList[0]
+        if input(f"Press 'y' if you try to mean '{newKey}': ").lower() == 'y':
+            synonym(newKey)
+            return
+        print('The key doesn\'t exist in the dictionary, Please check your input.. \n')
+
+    while search:
+        if searchKey != '':
+            synonym(searchKey.lower())
+            search = False
+        else: 
+            print('\nPlease enter a Value!\n')
+            searchKey = keyCall()
+    
+    
+def printTranslation(input):
+        for i in range(0, len(input)):
+            print(f'{i+1}. {input[i]}')
+
+if __name__ == '__main__':
+    main()
